0de949d1830987fd78205062ae138f8f1bb8520d
Logesh <logesh.jeppiaar@gmail.com>
2020-02-18 23:58:14 +0530

    log.jep-myfil cheangle

--- Test/src/com/logesh/HelloWorld.java
+++ Test/src/com/logesh/HelloWorld.java
@@ -3,6 +3,7 @@ package com.logesh;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("new one");
 		System.out.println("hello");
 		System.out.println("Helo world");
 	}
--- Test/src/com/logesh/MyFile.java
+++ Test/src/com/logesh/MyFile.java
@@ -1,2 +1,3 @@
 // new file
 //rtyu
+//change
